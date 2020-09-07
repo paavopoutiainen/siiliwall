4739ddf4d15b0a387ee9ff54856ebf54f295407e
Ilpo Jokinen <bgg300@myy.haaga-helia.fi>
2020-09-07 17:09:08 +0300

    LandingPage component created

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,15 @@
+import React, { useEffect, useState} from "react";
+import '../styles.css';
+
+const LandingPage = () => {
+    return (
+        <div className="landingPage">
+            <div className="landingPage__title"><h1>Welcome!</h1></div>
+            <div className="landingPage__boardList">
+                <h2>Boards:</h2>
+                
+            </div>
+        </div>
+    )
+}
+export default LandingPage
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-import BoardView from "../pages/BoardView"
+import React from 'react'
+import { Switch, Route } from 'react-router-dom'
+//import BoardView from "../pages/BoardView"
+import LandingPage from '../pages/LandingPage'
 import Home from "../pages/Home"
 export default function Routes() {
     return (
         <Switch>
-            <Route path="/" exact component={BoardView} />
+            <Route path="/" exact component={LandingPage} />
             <Route path="/home" exact component={Home} />
         </Switch>
-    );
+    )
 }
