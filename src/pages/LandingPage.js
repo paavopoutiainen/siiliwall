27df33201ec362524debf9fce2efa131f7548551
Ilpo Jokinen <bgg300@myy.haaga-helia.fi>
2020-09-08 15:16:21 +0300

    Landing page shows boards from db

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,13 +1,23 @@
 import React, { useEffect, useState} from "react";
+import { useQuery } from '@apollo/client'
+import { GET_ALL_BOARDS } from '../graphql/Queries'
 import '../styles.css';
 
 const LandingPage = () => {
+    const { loading, error, data } = useQuery(GET_ALL_BOARDS)
+
+    if (loading) return <p>Loading boards...</p>
+    if (error) return <p>Error</p>
+
     return (
         <div className="landingPage">
-            <div className="landingPage__title"><h1>Welcome!</h1></div>
+            <div className="landingPage__title">
+                <h1>Welcome!</h1>
+            </div>
             <div className="landingPage__boardList">
-                <h2>Boards:</h2>
-                
+                {data.allBoards.map(({ id, name }) => {
+                   return <div className="landingPage__boardList__boardButton"><a href={`/boards/${id}`} id={id} className="btn btn--white">{name}</a></div>
+                })}
             </div>
         </div>
     )
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,11 @@ import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 //import BoardView from "../pages/BoardView"
 import LandingPage from '../pages/LandingPage'
-import Home from "../pages/Home"
+
 export default function Routes() {
     return (
         <Switch>
-            <Route path="/" exact component={LandingPage} />
-            <Route path="/home" exact component={Home} />
+            <Route path="/" component={LandingPage} />
         </Switch>
     )
 }
