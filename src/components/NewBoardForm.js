343317356416f88ee393e1150b3c5df5a17467ca
Ilpo Jokinen <bgg300@myy.haaga-helia.fi>
2020-09-09 14:20:01 +0300

    Added material ui containers

---
+++ src/components/NewBoardForm.js
@@ -0,0 +1,9 @@
+import React, { useState } from 'react'
+
+const NewBoardForm = () => {
+    
+    return (
+       
+    )
+}
+export default NewBoardForm
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState} from "react";
+import { Grid, Button } from '@material-ui/core'
 import { Link } from 'react-router-dom'
 import { useQuery } from '@apollo/client'
 import { GET_ALL_BOARDS } from '../graphql/Queries'
 import '../styles.css';
-
 const LandingPage = () => {
     const { loading, error, data } = useQuery(GET_ALL_BOARDS)
 
@@ -11,7 +11,7 @@ const LandingPage = () => {
     if (error) return `Error: ${error.message}`
 
     return (
-        <div className="landingPage">
+        /*<div className="landingPage">
             <div className="landingPage__title">
                 <h1>Welcome!</h1>
             </div>
@@ -29,7 +29,38 @@ const LandingPage = () => {
                     )
                 })}
             </div>
-        </div>
+        </div>*/
+        <Grid 
+            container
+            direction="column"
+            alignItems="center"
+            className="landingPage"
+            spacing={5}
+        >
+            <Grid item className="landingPage__title" >
+                <h1>Welcome!</h1>
+            </Grid>
+            <Grid item className="landingPage__addButton">
+                <a href="#">Add Board</a>
+            </Grid>
+            <Grid 
+                container
+                direction="column" 
+                alignItems="center"
+                className="boardList"
+                spacing={2}
+            >
+                {data.allBoards.map(({id, name}) => {
+                    return (
+                        <Grid item className="boardList__button">
+                            <a href="#" className="btn btn--gray">
+                                <Link to={`/boards/${id}`}>{name}</Link>
+                            </a>
+                        </Grid>
+                    ) 
+                })}
+            </Grid>
+        </Grid>
     )
 }
 export default LandingPage
