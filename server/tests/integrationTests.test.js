795041d9c9493ec47abb6f8ddd3c8b98bf9d5be3
Paavo Poutiainen <paavopoutiainen90@gmail.com>
2020-09-10 16:45:03 +0300

    Couple more tests, testing query boardById, some unresolved problems with initializing database using beforeEach and beforeAll, tests dont always pass through though nothing is changed in between the runs

--- server/tests/integrationTests.test.js
+++ server/tests/integrationTests.test.js
@@ -3,40 +3,56 @@ const request = require('supertest')(app);
 const db = require("../models/index.js")
 const dummyData = require("../dummyData")
 
-describe("With initial test data in the database", () => {
-  beforeEach(async () => {
-    await db.sequelize.drop().then(() => db.sequelize.sync())
-      .then(() => {
-        dummyData.boards.forEach((board) => {
-          db.Board.create(board)
-        })
-        dummyData.columns.forEach((column) => {
-          db.Column.create(column)
-        })
-        dummyData.tasks.forEach((task) => {
-          db.Task.create(task)
-        })
-        dummyData.subtasks.forEach((subtask) => {
-          db.Subtask.create(subtask)
-        })
+describe("With initial test data in the database, queries", () => {
+  beforeAll(async () => {
+    return await db.sequelize.sync({force: true}).then(() => {
+      dummyData.boards.forEach(board => {
+        db.Board.create(board)
+      })
+      dummyData.columns.forEach(column => {
+        db.Column.create(column)
+      })
+      dummyData.tasks.forEach(task => {
+        db.Task.create(task)
+      })
+      dummyData.subtasks.forEach(subtask => {
+        db.Subtask.create(subtask)
+      })
     })
   })
 
-  test("allBoards query returns three boards", async () => {
-    const response = await request
-      .post("/graphql")
-      .send({ query: "{ allBoards{id name} }" })
-      
-    expect(response.body.data.allBoards).toEqual([{"id": "1", "name": "board1"}, {"id": "2", "name": "board2"}, {"id": "3", "name": "board3"}])
+  test("Boards are returned as JSON", async () => {
+      await request
+        .post("/graphql")
+        .send({ query: "{ allBoards { id name } }" })
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+    
   })
 
+  test("allBoards query returns three boards in an array", async () => {
+      const response = await request
+        .post("/graphql")
+        .send({ query: "{ allBoards { id name columnOrder } }" })
+        .expect('Content-Type', /application\/json/)
+
+      const allBoards = response.body.data.allBoards
+      expect(allBoards).toHaveLength(3)
+  })
+
+  test("boardById query returns one board with the given id", async () => {
+    const response = await request
+      .post("/graphql")
+      .send({ query: '{ boardById(id: "1") { id name } }' })
+
+    const board = response.body.data.boardById
+    expect(board.id).toBe("1")  
+  })
 })
-
-
 
 afterAll(async () => {
   closeHttpServer()
   await db.sequelize.drop()
-  await db.sequelize.close()
+  return await db.sequelize.close()
 })
 
