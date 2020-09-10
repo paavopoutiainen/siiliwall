0811c4f1f492203f54eb2c30abf4763d9b5c5e5f
Ilpo Jokinen <bgg300@myy.haaga-helia.fi>
2020-09-10 11:48:33 +0300

    Created NewBoardForm component. The LandingPage is now responsive with Material ui containers.

--- src/components/NewBoardForm.js
+++ src/components/NewBoardForm.js
@@ -1,9 +1,39 @@
 import React, { useState } from 'react'
+import { Dialog, DialogActions, DialogContent, DialogContentText,
+     DialogTitle, TextField, Button } from '@material-ui/core'
 
-const NewBoardForm = () => {
-    
+const NewBoardForm = ({ setOpen, open }) => {
+
+    function handleClose() {
+        setOpen(false)
+    }
+
     return (
-       
+       <div>
+          <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+            <DialogTitle id="form-dialog-title">New board</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                      Please enter the credentials for the board.
+                    </DialogContentText>
+                    <TextField
+                        autoFocus
+                        id="name"
+                        label="Name"
+                        type="text"
+                        fullWidth
+                    />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose} color="primary">
+                        Cancel
+                    </Button>
+                    <Button onClick={handleClose} color="primary">
+                        Add
+                    </Button>
+                </DialogActions>
+            </Dialog>
+       </div>
     )
 }
 export default NewBoardForm
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,64 +1,67 @@
-import React, { useEffect, useState} from "react";
+import React, { useEffect, useState} from "react"
 import { Grid, Button } from '@material-ui/core'
 import { Link } from 'react-router-dom'
+import NewBoardForm from '../components/NewBoardForm'
 import { useQuery } from '@apollo/client'
 import { GET_ALL_BOARDS } from '../graphql/Queries'
-import '../styles.css';
+import { useGradientBtnStyles } from '@mui-treasury/styles/button/gradient'
+import '../styles.css'
+
 const LandingPage = () => {
     const { loading, error, data } = useQuery(GET_ALL_BOARDS)
+    const [open, setOpen ] = useState(false)
+
+  
+    function handleClickOpen () {
+        setOpen(true)
+    }
+    const styles = useGradientBtnStyles()
+    const boardButton = {
+        minWidth: 200,
+        background: 'linear-gradient(to right, #FFC371, #FF5F6D)'
+    }
 
     if (loading) return <p>Loading boards...</p>
     if (error) return `Error: ${error.message}`
 
     return (
-        /*<div className="landingPage">
-            <div className="landingPage__title">
-                <h1>Welcome!</h1>
-            </div>
-            <div className="landingPage__add">
-                <a href="#" className="btn btn--white">Add Board</a>
-            </div>
-            <div className="landingPage__boardList">
-                {data.allBoards.map(({ id, name }) => {
-                    return (
-                        <div className="landingPage__boardList__boardButton">
-                            <a href="#" className="btn btn--gray">
-                                <Link to={`/boards/${id}`}>{name}</Link>
-                            </a>
-                        </div>
-                    )
-                })}
-            </div>
-        </div>*/
         <Grid 
             container
             direction="column"
             alignItems="center"
+            justify="center"
             className="landingPage"
-            spacing={5}
+            spacing={7}
         >
+            { open && 
+                <NewBoardForm setOpen={setOpen} open={open}/>
+            }
             <Grid item className="landingPage__title" >
                 <h1>Welcome!</h1>
             </Grid>
-            <Grid item className="landingPage__addButton">
-                <a href="#">Add Board</a>
+            <Grid item>
+                <Button onClick={handleClickOpen}>
+                    <Link className="landingPage__addButton">Add Board</Link>
+                </Button>
             </Grid>
-            <Grid 
-                container
-                direction="column" 
-                alignItems="center"
-                className="boardList"
-                spacing={2}
-            >
-                {data.allBoards.map(({id, name}) => {
-                    return (
-                        <Grid item className="boardList__button">
-                            <a href="#" className="btn btn--gray">
-                                <Link to={`/boards/${id}`}>{name}</Link>
-                            </a>
-                        </Grid>
-                    ) 
-                })}
+            <Grid item className="landingPage__boardList" >
+                <Grid 
+                    container
+                    direction="column" 
+                    alignItems="center"
+                    className="boardList"
+                    spacing={2}
+                >
+                    {data.allBoards.map(({id, name}) => {
+                        return (
+                            <Grid item className="boardList__button">
+                                <Button fullWidth={true} className="gradientButton">
+                                    <Link to={`/boards/${id}`} className="boardList__button__link">{name}</Link>
+                                </Button>
+                            </Grid>
+                        ) 
+                    })}
+                </Grid>
             </Grid>
         </Grid>
     )
