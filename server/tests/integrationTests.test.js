6cdb5842cd0ceb0fc5366a1963b3e15c74fb9a54
Paavo Poutiainen <paavopoutiainen90@gmail.com>
2020-09-11 18:36:36 +0300

    Fixed eslint problems

--- server/tests/integrationTests.test.js
+++ server/tests/integrationTests.test.js
@@ -1,46 +1,41 @@
-const { app } = require("../src/index.js")
-const request = require('supertest')(app);
-const { initializeDb, afterTests } = require("./utils");
-const db = require("../models/index.js");
+const supertest = require('supertest')
+const { app } = require('../src/index.js')
+const { initializeDb, afterTests } = require('./utils')
 
-describe("With initial test data in the database, queries", () => {
-  /*
+const request = supertest(app)
+
+describe('With initial test data in the database, queries', () => {
+    /*
     Reinitialize the database before each test in this describe block
   */
-  beforeEach(async () => {
-    return initializeDb()
-  })
+    beforeEach(() => initializeDb())
 
-  test("Boards are returned as JSON", async () => {
-      await request
-        .post("/graphql")
-        .send({ query: "{ allBoards { id name } }" })
-        .expect(200)
-        .expect('Content-Type', /application\/json/)
-    
-  })
+    test('Boards are returned as JSON', async () => {
+        await request
+            .post('/graphql')
+            .send({ query: '{ allBoards { id name } }' })
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+    })
 
-  test("allBoards query returns three boards in an array", async () => {
-      const response = await request
-        .post("/graphql")
-        .send({ query: "{ allBoards { id name columnOrder } }" })
-        .expect('Content-Type', /application\/json/)
+    test('allBoards query returns three boards in an array', async () => {
+        const response = await request
+            .post('/graphql')
+            .send({ query: '{ allBoards { id name columnOrder } }' })
+            .expect('Content-Type', /application\/json/)
 
-      const allBoards = response.body.data.allBoards
-      expect(allBoards).toHaveLength(3)
-  })
+        const { allBoards } = response.body.data
+        expect(allBoards).toHaveLength(3)
+    })
 
-  test("boardById query returns one board with the given id", async () => {
-    const response = await request
-      .post("/graphql")
-      .send({ query: '{ boardById(id: "1") { id name } }' })
+    test('boardById query returns one board with the given id', async () => {
+        const response = await request
+            .post('/graphql')
+            .send({ query: '{ boardById(id: "1") { id name } }' })
 
-    const board = response.body.data.boardById
-    expect(board.id).toBe("1")  
-  })
+        const board = response.body.data.boardById
+        expect(board.id).toBe('1')
+    })
 })
 
-afterAll(async () => {
-  return afterTests()
-})
-
+afterAll(() => afterTests())
--- server/tests/utils.js
+++ server/tests/utils.js
@@ -1,40 +1,45 @@
-const db = require("../models/index.js")
-const dummyData = require("../dummyData")
-const { closeHttpServer } = require("../src/index.js")
+const db = require('../models/index.js')
+const dummyData = require('../dummyData')
+const { closeHttpServer } = require('../src/index.js')
 
 /*
-  Drop all the tables, sync the models with the database, insert some testdata and return a promise when everything has resolved
+  Drop all the tables, sync the models with the database,
+  insert some testdata and return a promise when everything has resolved
 */
 const initializeDb = async () => {
-  await db.sequelize.sync({ force: true })
-  await Promise.all(
-    dummyData.boards.map(async board => {
-      return await db.Board.create(board)
-    })
-  )
-  await Promise.all(
-    dummyData.columns.map(async column => {
-      return await db.Column.create(column)
-    })
-  )
-  await Promise.all(
-    dummyData.tasks.map(async task => {
-      return await db.Task.create(task)
-    })
-  )
-  await Promise.all(
-    dummyData.subtasks.map(async subtask => {
-      return await db.Subtask.create(subtask)
-    })
-  )
-return new Promise((resolve) => resolve())
+    await db.sequelize.sync({ force: true })
+    await Promise.all(
+        dummyData.boards.map(async (board) => {
+            const resolved = await db.Board.create(board)
+            return resolved
+        }),
+    )
+    await Promise.all(
+        dummyData.columns.map(async (column) => {
+            const resolved = await db.Column.create(column)
+            return resolved
+        }),
+    )
+    await Promise.all(
+        dummyData.tasks.map(async (task) => {
+            const resolved = await db.Task.create(task)
+            return resolved
+        }),
+    )
+    await Promise.all(
+        dummyData.subtasks.map(async (subtask) => {
+            const resolved = await db.Subtask.create(subtask)
+            return resolved
+        }),
+    )
+    return new Promise((resolve) => resolve())
 }
 
 const afterTests = async () => {
-  closeHttpServer()
-  await db.sequelize.drop()
-  await db.sequelize.close()
-  return new Promise((resolve) => resolve())
+    closeHttpServer()
+    await db.sequelize.drop()
+    await db.sequelize.close()
+    return new Promise((resolve) => resolve())
 }
 
 module.exports = { initializeDb, afterTests }
