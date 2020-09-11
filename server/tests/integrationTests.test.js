f3ab7de91bc2da15f1a8c73ae4878d003d4e817e
Paavo Poutiainen <paavopoutiainen90@gmail.com>
2020-09-11 20:34:32 +0300

    Added couple integration tests for addBoard mutation

--- server/tests/integrationTests.test.js
+++ server/tests/integrationTests.test.js
@@ -1,6 +1,8 @@
 const supertest = require('supertest')
 const { app } = require('../src/index.js')
-const { initializeDb, afterTests } = require('./utils')
+const {
+    initializeDb, afterTests, boardsInTheDb, initialBoards,
+} = require('./utils')
 
 const request = supertest(app)
 
@@ -57,10 +59,33 @@ describe('With initial test data in the database, queries', () => {
     test('subtasks array of task can be accessed in the response of taskById query', async () => {
         const response = await request
             .post('/graphql')
-            .send({ query: '{ taskById(id: "3") { id title subtasks {id} } }' })
+            .send({ query: '{ taskById(id: "3") { id title subtasks { id } } }' })
 
         const task = response.body.data.taskById
         expect(Array.isArray([task.subtasks])).toBe(true)
+    })
+})
+
+describe('mutations', () => {
+    /*
+    Reinitialize the database before each test in this describe block
+    */
+    beforeEach(() => initializeDb())
+
+    test('addBoard mutation responds with JSON', async () => {
+        await request
+            .post('/graphql')
+            .send({ query: '{ mutation addBoard(name: "newBoard") { id } }' })
+            .expect('Content-Type', /application\/json/)
+    })
+
+    test('board can be added using addBoard mutation', async () => {
+        await request
+            .post('/graphql')
+            .send({ query: 'mutation{ addBoard(name: "newBoard") { id } }' })
+
+        const boardsAtEnd = await boardsInTheDb()
+        expect(initialBoards.length + 1).toEqual(boardsAtEnd.length)
     })
 })
 
