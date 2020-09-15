d6e7d167c53003ff687744347425dd425bfe7eae
Paavo Poutiainen <paavopoutiainen90@gmail.com>
2020-09-15 22:19:23 +0300

    Added mutation and resolvers abling moving a task between columns, basically changing the columnId of the moved task and changing the columnOrderNumbers of the tasks in the affected columns

--- server/src/datasources/BoardService.js
+++ server/src/datasources/BoardService.js
@@ -183,7 +183,7 @@ class BoardService {
     }
 
     // Loop through tasks and set the new columnOrderNumber for each using the index of the array
-    async reOrderColumnsTasks(newOrderArray, columnId) {
+    async reOrderTasksOfColumn(newOrderArray, columnId) {
         await Promise.all(newOrderArray.map(async (id, index) => {
             const task = await this.store.Task.findByPk(id)
             task.columnOrderNumber = index
@@ -191,6 +191,12 @@ class BoardService {
         }))
         const column = await this.store.Column.findByPk(columnId)
         return column
+    }
+
+    async changeTasksColumnId(taskId, columnId) {
+        const task = await this.store.Task.findByPk(taskId)
+        task.columnId = columnId
+        await task.save()
     }
 }
 
