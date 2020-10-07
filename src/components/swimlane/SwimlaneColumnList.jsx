72d6844ae582159a3c927e24ac5c598484dd4525
Ilpo Jokinen <bgg300@myy.haaga-helia.fi>
2020-10-07 14:05:04 +0300

    Modified the SwimlaneColumnList component

--- src/components/swimlane/SwimlaneColumnList.jsx
+++ src/components/swimlane/SwimlaneColumnList.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import { Grid } from '@material-ui/core'
 import { swimlaneStyles } from '../../styles/styles'
+import SwimlaneColumn from './SwimlaneColumn'
 
-const SwimlaneColumnList = () => {
+const SwimlaneColumnList = ({ swimlaneColumns }) => {
     const classes = swimlaneStyles()
 
     return (
-        <Grid container direction="row" spacing={2} classes={{ root: classes.columnList }}>
-
+        <Grid container direction="row" spacing={2} >
+            {swimlaneColumns.map((colObj, index) =>
+                <Grid item key={index}><SwimlaneColumn swimlaneColumn={colObj} /></Grid>
+            )}
         </Grid>
     )
 }
