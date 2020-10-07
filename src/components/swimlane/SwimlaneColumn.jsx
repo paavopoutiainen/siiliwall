d3bda7ca80b7c7b1712295a22c7340f72e4dd8e2
Paavo Poutiainen <paavopoutiainen90@gmail.com>
2020-10-07 14:47:03 +0300

    SwimlaneColumn component renders subtasks in correct order

--- src/components/swimlane/SwimlaneColumn.jsx
+++ src/components/swimlane/SwimlaneColumn.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import { Grid } from '@material-ui/core'
 import { swimlaneStyles } from '../../styles/styles'
+import Subtask from '../subtask/Subtask'
 
 const SwimlaneColumn = ({ swimlaneColumn }) => {
+    const { subtaskOrder, subtasks } = swimlaneColumn
     const classes = swimlaneStyles()
-    // swimlaneColumnin sisälle täytyy saada kyseisen palasen ticketOrder
-    console.log('here', swimlaneColumn)
+
+    const subtasksInOrder = subtaskOrder.map((obj) => subtasks.find((subtask) => subtask.id === obj.ticketId))
     return (
-        <Grid container />
+        <Grid container>
+            {subtasksInOrder.map((subtask, index) => (
+                <Grid item key={subtask.id}>
+                    <Subtask subtask={subtask} index={index} columnId={swimlaneColumn.id} />
+                </Grid>
+            ))}
+        </Grid>
+
     )
 }
 export default SwimlaneColumn
