a4a4ecfa4620601fc60607b743029ede5129b978
Ilpo Jokinen <bgg300@myy.haaga-helia.fi>
2020-10-08 16:39:05 +0300

    changed the moveSwimlane mutation

--- src/graphql/swimlane/swimlaneQueries.js
+++ src/graphql/swimlane/swimlaneQueries.js
@@ -2,6 +2,6 @@ import { gql } from '@apollo/client'
 
 export const MOVE_SWIMLANE = gql`
     mutation moveSwimlane($swimlaneOrderNumber: [ID!]!, $boardId: ID!) {
-        moveSwimlane(boardId: $boardId, newSimlaneOrder: $swimlaneOrderNumber)
+        moveSwimlane(boardId: $boardId, newSwimlaneOrder: $swimlaneOrderNumber)
     }
 `
