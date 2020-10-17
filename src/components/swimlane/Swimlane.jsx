ff666639759747e69f5fcae3749f14fe0daada16
Paavo Poutiainen <paavopoutiainen90@gmail.com>
2020-10-17 18:29:24 +0300

    Added remove prio button for swimlane so that user can see if the swimlane has been given special priority in terms of its place in the swimlane view. By pressing the button user can put the swimlane into its normal place

--- src/components/swimlane/Swimlane.jsx
+++ src/components/swimlane/Swimlane.jsx
@@ -1,12 +1,57 @@
+/* eslint-disable array-callback-return */
 import React from 'react'
 import { Grid } from '@material-ui/core'
+import { Draggable } from 'react-beautiful-dnd'
+import { useApolloClient } from '@apollo/client'
 import { swimlaneStyles } from '../../styles/styles'
 import SwimlaneHeader from './SwimlaneHeader'
 import SwimlaneColumnList from './SwimlaneColumnList'
-import { Draggable } from 'react-beautiful-dnd'
+import {
+    PRIORITIZED_AND_SWIMLANEORDERNUMBER,
+} from '../../graphql/fragments'
+import useUnPrioritizeTask from '../../graphql/task/hooks/useUnPrioritizeTask'
 
-const Swimlane = ({ task, index }) => {
+const Swimlane = ({ tasksInOrder, task, index }) => {
     const classes = swimlaneStyles()
+    const client = useApolloClient()
+    const [unPrioritizeTask] = useUnPrioritizeTask()
+
+    const removePrioritization = () => {
+        client.writeFragment({
+            id: `Task:${task.id}`,
+            fragment: PRIORITIZED_AND_SWIMLANEORDERNUMBER,
+            data: {
+                prioritized: false,
+                swimlaneOrderNumber: null,
+            },
+        })
+        // When swimlane/task gets unprioritized we have to change the swimlaneOrderNumber of
+        // all the prioritized tasks beneath the swimlane
+        const prioritizedTasksBeneathTheSwimlane = tasksInOrder
+            .filter((taskObj) => taskObj.prioritized)
+            .filter((taskObj) => taskObj.id !== task.id)
+            .filter((taskObj) => taskObj.swimlaneOrderNumber > task.swimlaneOrderNumber)
+        // Update the cache
+        prioritizedTasksBeneathTheSwimlane.map((taskObj) => {
+            client.writeFragment({
+                id: `Task:${taskObj.id}`,
+                fragment: PRIORITIZED_AND_SWIMLANEORDERNUMBER,
+                data: {
+                    prioritized: true,
+                    swimlaneOrderNumber: taskObj.swimlaneOrderNumber - 1,
+                },
+            })
+        })
+        // SwimlaneOrderNumbers need to be updatetd to the database aswell
+        const prioritizedTasksBeneathTheSwimlaneIds = prioritizedTasksBeneathTheSwimlane.map((taskObj) => taskObj.id)
+
+        unPrioritizeTask({
+            variables: {
+                id: task.id,
+                prioritizedTaskIds: prioritizedTasksBeneathTheSwimlaneIds,
+            },
+        })
+    }
     return (
         <Draggable draggableId={task.id} index={index}>
             {(provided) => (
@@ -20,6 +65,7 @@ const Swimlane = ({ task, index }) => {
                     ref={provided.innerRef}
                 >
                     <Grid item><SwimlaneHeader taskName={task.title} /></Grid>
+                    {task.prioritized ? <Grid item><button onClick={() => removePrioritization()}>remove prio</button></Grid> : null}
                     <Grid item><SwimlaneColumnList swimlaneColumns={task.swimlaneColumns} taskId={task.id} /></Grid>
                 </Grid>
             )}
