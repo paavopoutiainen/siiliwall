2e18950fca215cfd6bc456f203f45e5891999300
Paavo Poutiainen <paavopoutiainen90@gmail.com>
2020-10-29 17:21:11 +0200

    Added subscriptions web socket link and a function that checks which connection http or web socket to use depending on the query

--- src/apollo.js
+++ src/apollo.js
@@ -1,8 +1,35 @@
 /* eslint-disable import/prefer-default-export */
-import { ApolloClient, InMemoryCache } from '@apollo/client'
+import {
+    ApolloClient, InMemoryCache, split, HttpLink,
+} from '@apollo/client'
+import { WebSocketLink } from '@apollo/client/link/ws'
+import { getMainDefinition } from '@apollo/client/utilities'
+
+const httpLink = new HttpLink({
+    uri: 'http://localhost:4001/graphql',
+})
+
+const wsLink = new WebSocketLink({
+    uri: 'ws://localhost:4001/subscriptions',
+    options: {
+        reconnect: true,
+    },
+})
+
+const splitLink = split(
+    ({ query }) => {
+        const definition = getMainDefinition(query)
+        return (
+            definition.kind === 'OperationDefinition'
+      && definition.operation === 'subscription'
+        )
+    },
+    wsLink,
+    httpLink,
+)
 
 export const client = new ApolloClient({
-    uri: 'http://localhost:4001/graphql',
+    link: splitLink,
     cache: new InMemoryCache({
         typePolicies: {
             Board: {
