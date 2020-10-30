0fd19924481e9034fcbaba5edc7d464ff03da498
Paavo Poutiainen <paavopoutiainen90@gmail.com>
2020-10-30 22:09:58 +0200

    Removed unused imported fragment form AlertBox

--- src/components/AlertBox.jsx
+++ src/components/AlertBox.jsx
@@ -8,7 +8,7 @@ import { useMutation, useApolloClient } from '@apollo/client'
 import { boardPageStyles } from '../styles/styles'
 import { DELETE_COLUMN } from '../graphql/column/columnQueries'
 import {
-    COLUMNORDER, TICKETORDER, COLUMNORDER_AND_COLUMNS, TICKETORDER_AND_TASKS, TICKETORDER_AND_SUBTASKS, SWIMLANE_ORDER,
+    COLUMNORDER, COLUMNORDER_AND_COLUMNS, TICKETORDER_AND_TASKS, TICKETORDER_AND_SUBTASKS, SWIMLANE_ORDER,
 } from '../graphql/fragments'
 import { DELETE_TASK } from '../graphql/task/taskQueries'
 import useArchiveTask from '../graphql/task/hooks/useArchiveTask'
