d205700acdeee36bbf69821e7efe8f12ac047366
Paavo Poutiainen <paavopoutiainen90@gmail.com>
2020-11-02 13:15:11 +0200

    Changed TicketList component to use React.memo instead of PureComponent

--- src/components/TicketList.jsx
+++ src/components/TicketList.jsx
@@ -3,44 +3,42 @@ import { Grid } from '@material-ui/core'
 import Task from './task/Task'
 import Subtask from './subtask/Subtask'
 
-class TicketList extends React.PureComponent {
-    render() {
-        const {
-            ticketOrder, tasks, subtasks, columnId, boardId,
-        } = this.props
-        const ticketsInOrder = ticketOrder.map((obj) => {
-            let foundTicket
-            if (obj.type === 'task') {
-                foundTicket = tasks.find((task) => task.id === obj.ticketId)
-                foundTicket = { ...foundTicket, type: 'task' }
-            } else if (obj.type === 'subtask') {
-                foundTicket = subtasks.find((subtask) => subtask.id === obj.ticketId)
-                foundTicket = { ...foundTicket, type: 'subtask' }
-            }
-            return foundTicket
-        })
-        return (
-            <Grid container direction="column" spacing={2}>
-                {ticketsInOrder.map((ticket, index) => {
-                    let component
-                    if (ticket.type === 'task') {
-                        component = (
-                            <Grid item key={ticket.id}>
-                                <Task index={index} task={ticket} columnId={columnId} boardId={boardId} />
-                            </Grid>
-                        )
-                    } else if (ticket.type === 'subtask') {
-                        component = (
-                            <Grid item key={ticket.id}>
-                                <Subtask key={ticket.id} index={index} subtask={ticket} columnId={columnId} />
-                            </Grid>
-                        )
-                    }
-                    return component
-                })}
-            </Grid>
-        )
-    }
+const TicketList = ({
+    ticketOrder, tasks, subtasks, columnId, boardId,
+}) => {
+    const ticketsInOrder = ticketOrder.map((obj) => {
+        let foundTicket
+        if (obj.type === 'task') {
+            foundTicket = tasks.find((task) => task.id === obj.ticketId)
+            foundTicket = { ...foundTicket, type: 'task' }
+        } else if (obj.type === 'subtask') {
+            foundTicket = subtasks.find((subtask) => subtask.id === obj.ticketId)
+            foundTicket = { ...foundTicket, type: 'subtask' }
+        }
+        return foundTicket
+    })
+    return (
+        <Grid container direction="column" spacing={2}>
+            {ticketsInOrder.map((ticket, index) => {
+                let component
+                if (ticket.type === 'task') {
+                    component = (
+                        <Grid item key={ticket.id}>
+                            <Task index={index} task={ticket} columnId={columnId} boardId={boardId} />
+                        </Grid>
+                    )
+                } else if (ticket.type === 'subtask') {
+                    component = (
+                        <Grid item key={ticket.id}>
+                            <Subtask key={ticket.id} index={index} subtask={ticket} columnId={columnId} />
+                        </Grid>
+                    )
+                }
+
+                return component
+            })}
+        </Grid>
+    )
 }
 
-export default TicketList
+export default React.memo(TicketList)
