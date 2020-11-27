ad27c48a439fdcfae2a0d2a6716b9576de3dde53
Jussi Lemmetyinen <jussi.lemmetyinen@siili.com>
2020-11-27 12:52:38 +0200

    Feature/add env script

---
+++ server/setup_env.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo DB_USERNAME="$DB_USERNAME" >> .env
+echo DB_PASSWORD="$DB_PASSWORD" >> .env
+echo DB_DATABASE="$DB_DATABASE" >> .env
+echo DB_HOST="$DB_HOST" >> .env
+echo PORT="$PORT" >> .env
+
